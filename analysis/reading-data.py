97fe97c483efc787add6e177e0a42715e5901aba
anmom14a <anmom14a@teleco.upv.es>
2023-03-24 16:48:27 +0200

    Map creation with values from 2000

--- analysis/reading-data.py
+++ analysis/reading-data.py
@@ -1,5 +1,6 @@
-
 import pandas as pd
+import geopandas
+import folium 
 
 # Clean Global land temperature dataset
 df = pd.read_csv('data\land-data\GlobalLandTemperaturesByCountry.csv')
@@ -47,10 +48,36 @@ df_pop.sort_values(by=['Country'], inplace=True)
 df_pop['Year'] = df_pop['Year'].astype(int)
 
 df_merged = df_merged.merge(df_pop, on=['Country', 'Year'], how='inner')
-df_merged
 
 # Save merge dataset in clean-data folder
 df_merged.to_csv('data\clean-data\merged_data.csv', index=False)
 
-## Geopandas
-# link: https://www.relataly.com/visualize-covid-19-data-on-a-geographic-heat-maps/291/
+# Plot 2010 Average temperature values in a map
+df_2000 = pd.DataFrame(df_merged[df_merged.Year == 2000])
+df_2000.drop(columns = ["Population", "AverageTemperature"], inplace=True)
+
+## import geopandas dataset for map
+world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
+print(world.head())
+
+data_map = world.merge(df_2000, how = "left", left_on=["name"], right_on=["Country"])
+
+my_map = folium.Map()
+
+folium.Choropleth(
+    geo_data=data_map,
+    name='choropleth',
+    data=data_map,
+    columns=['Country', 'GDP'],
+    key_on='feature.properties.name',
+    fill_color='OrRd',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Población del mundo a tope'
+
+).add_to(my_map)
+
+my_map.save('gdp.html')
+
+## Los valores del mapa seguramente estén mal, hay que mirar que salen bien los datos y si existe una 
+## librería que pueda juntar bien los nombres de los países
